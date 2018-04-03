fcfb151d5563ed30862a7f416cd0d28dd28fad75
vsogawa <36315323+vsogawa@users.noreply.github.com>
2018-04-02 22:55:18 -0700

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -7,22 +7,32 @@ class FEsprite {
         this.sad = "img/"+character+"_sad.png",
         this.shock = "img/"+character+"_shock.png"
         if (character === "elise") {
-            this.diamond = "For me?  Yippee!";
+            this.diamond = "For me?  Yippee!"
             this.bomb = "That's not a funny joke! I won't go easy on you, you know!"
             this.lightning = "Oh wow, that was so loud! Must have been a close one."
+            this.throne = "Why do I need to sit here? I'm pretty busy you know."
             this.name = "Elise"
         }
         if (character === "takumi") {
-            this.diamond = "Hmph. You're not so bad after all.";
+            this.diamond = "Hmph. You're not so bad after all."
             this.bomb = "I knew you were Nohrian scum all along! Prepare to die by my hands!"
             this.lightning = "Hnng!"
+            this.throne = "This throne reminds me of my mother..."
             this.name = "Takumi"
         }
         if (character === "felicia") {
-            this.diamond = "Th-thank you for your kindness!";
+            this.diamond = "Th-thank you for your kindness!"
             this.bomb = "If that is what you desire, I will follow you until the end."
-            this.lightning = "Oh my! Are you okay?"
+            this.lightning = "Oh my! A-Are you okay?"
+            this.throne = "??"
             this.name = "Felicia"
+        }
+        if (character === "garon") {
+            this.diamond = "......."
+            this.bomb = ".............I wouldn't have it any other way."
+            this.lightning = "..............."
+            this.throne = "*GROANS OF INCREASING DISCOMFORT*"
+            this.name = "Garon"
         }
     }
 
@@ -31,6 +41,7 @@ class FEsprite {
 let elise = new FEsprite("elise");
 let takumi = new FEsprite("takumi");
 let felicia = new FEsprite("felicia");
+let garon = new FEsprite("garon");
 let currentPageChara = "";
 
 let charaArray = [elise, takumi, felicia];
@@ -66,4 +77,17 @@ $(".fa-bomb").on("click", function(){
 $(".fa-bolt").on("click", function(){
     $("#sprite").empty().append("<img id = \"chara\" src=" + currentPageChara.shock + ">");
     $("#text").empty().append(currentPageChara.lightning);
+})
+
+$(".fa-wheelchair").on("click", function(){
+    if (currentPageChara === garon) {
+        $("#sprite").empty().append("<img id = \"chara\" src = garon_melt.png>");
+    }
+    if (currentPageChara === takumi || currentPageChara === felicia) {
+        $("#sprite").empty().append("<img id = \"chara\" src=" + currentPageChara.sad + ">");
+    }
+    if (currentPageChara === elise) {
+        $("#sprite").empty().append("<img id = \"chara\" src=" + currentPageChara.grumpy + ">");
+    }
+    $("#text").empty().append(currentPageChara.throne);
 })
